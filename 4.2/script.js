8dbb2e5619f3034619c3df9af7d62f91182324f8
Luegreen <Luegreen@gmail.com>
2016-10-07 10:20:05 -0400

    Ah, fixed the function

--- 4.2/script.js
+++ 4.2/script.js
@@ -3,9 +3,16 @@
  */
 
 //Create a function such that, given argument arr, an array, it returns the same array in reverse order
+//The following works but is not a function called reverseArray.
+//var arr =[1,2,3,4,5];
+//arr.reverse(); 
+//console.log(arr);
 
-var arr =[1,2,3,4,5];
-arr.reverse(); 
-console.log(arr);
+function reverseArray(i) {
+    var i;
+    i.reverse();
+    console.log(i);
+}
 
+console.log( reverseArray([1,2,3,4,5]) )
 //console.log( reverseArray([1,2,3,4,5]) ); //should log [5,4,3,2,1]
